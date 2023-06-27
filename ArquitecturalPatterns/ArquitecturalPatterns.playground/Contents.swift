5b6f4047b6ba8488728f480541be9609b7850697
Danni Brito <daniebro.5@gmail.com>
2023-06-26 22:41:23 -0500

    Added architectural descriptions

--- ArquitecturalPatterns/ArquitecturalPatterns.playground/Contents.swift
+++ ArquitecturalPatterns/ArquitecturalPatterns.playground/Contents.swift
@@ -10,19 +10,29 @@
 
  En resumen, los patrones de diseño se centran en la solución de problemas a nivel de componentes individuales, mientras que los patrones de arquitectura se enfocan en la estructura y organización general de una aplicación o sistema. Ambos tipos de patrones son herramientas importantes en el desarrollo de software y se complementan entre sí para crear sistemas eficientes, escalables y mantenibles.
  
+ ********** Patrones de Arquitectura Comunes ************
  
- Todos tienen un Model por debajo - Implícito
- 
- +Complejo +Testeo
- 
- MVC -> Model "ViewController" - Apps pequeñas
- - Massive ViewController
- 
- MVVM -> Model - View - ViewModel
- - El usuario mira la vista, pero interactúa con el ViewModel, programación reactiva muy buena
- 
- VIPER -> View - Interactor(lógica del negocio) - "Presenter (se suele omitir, y es necesario en View Complejas)" - Router (BUILDER)
- 
- RIBs (es un lego) -> Router - Interactor - Builder
- - Router - desde donde puede venir o instanciarse
- - El
+ Aquí tienes una descripción breve de los patrones de arquitectura MVC, MVVM, VIPER y RIBs, junto con algunas ventajas y desventajas asociadas a cada uno:
+
+ 1. MVC (Model-View-Controller):
+    - Descripción: Separación de responsabilidades en tres componentes: el Modelo (lógica de negocio y datos), la Vista (interfaz de usuario) y el Controlador (mediador entre el Modelo y la Vista).
+    - Ventajas: Simplicidad, facilidad de comprensión, ampliamente utilizado, buen enfoque para aplicaciones pequeñas y sencillas.
+    - Desventajas: Dificultades para escalar en aplicaciones grandes, el Controlador puede volverse pesado y difícil de mantener, acoplamiento entre el Controlador y la Vista.
+
+ 2. MVVM (Model-View-ViewModel):
+    - Descripción: Separación de responsabilidades en tres componentes: el Modelo (lógica de negocio y datos), la Vista (interfaz de usuario) y el ViewModel (encargado de exponer los datos y acciones para la Vista).
+    - Ventajas: Separación clara de responsabilidades, facilita la reutilización de código, soporte para enlace de datos (data binding), buena escalabilidad, facilita las pruebas unitarias.
+    - Desventajas: Mayor complejidad en comparación con MVC, curva de aprendizaje inicial, puede ser excesivo para aplicaciones pequeñas y sencillas.
+
+ 3. VIPER (View-Interactor-Presenter-Entity-Router):
+    - Descripción: Separación de responsabilidades en cinco componentes: la Vista (interfaz de usuario), el Interactor (lógica de negocio), el Presenter (mediador entre la Vista y el Interactor), la Entidad (datos) y el Router (navegación entre módulos).
+    - Ventajas: Alta modularidad y escalabilidad, facilita las pruebas unitarias, separación clara de responsabilidades, mejora el mantenimiento y la colaboración en equipos grandes.
+    - Desventajas: Mayor complejidad y cantidad de código en comparación con MVC y MVVM, curva de aprendizaje inicial, puede ser excesivo para aplicaciones pequeñas y sencillas.
+
+ 4. RIBs (Router-Interactor-Builder):
+    - Descripción: Basado en el concepto de construcción de bloques, donde los componentes se componen jerárquicamente. Cada módulo tiene su propio Router, Interactor y Builder.
+    - Ventajas: Modularidad extrema, fácil extensibilidad y reutilización, mejora el mantenimiento y la colaboración en equipos grandes, permite un enfoque LEGO-like en el desarrollo de aplicaciones.
+    - Desventajas: Mayor complejidad y curva de aprendizaje inicial, más adecuado para aplicaciones grandes y complejas, puede generar un número significativo de archivos y clases.
+
+ Es importante tener en cuenta que cada patrón de arquitectura tiene su lugar y su aplicabilidad depende del contexto y los requisitos del proyecto. Las ventajas y desventajas mencionadas son generales y pueden variar según la situación. Es recomendable evaluar cuidadosamente las necesidades y características del proyecto antes de seleccionar un patrón de arquitectura.
+*/
