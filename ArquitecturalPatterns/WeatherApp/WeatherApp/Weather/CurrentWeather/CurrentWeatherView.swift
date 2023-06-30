35f9ade59fab9cf2482ac8aeb4b38bd4509d774f
Danni <daniebro.5@gmail.com>
2023-06-30 12:32:39 -0500

    Refactor CurrentWeatherView to simplify code structure and improve readability

--- ArquitecturalPatterns/WeatherApp/WeatherApp/Weather/CurrentWeather/CurrentWeatherView.swift
+++ ArquitecturalPatterns/WeatherApp/WeatherApp/Weather/CurrentWeather/CurrentWeatherView.swift
@@ -8,46 +8,36 @@ import SwiftUI
 struct CurrentWeatherView: View {
     
     @ObservedObject var viewModel: CurrentWeatherViewModel
-    @State var refresh: Bool = false
     
     init(viewModel: CurrentWeatherViewModel) {
         self.viewModel = viewModel
     }
     
     var body: some View {
-        VStack {
-            List(content: content)
-                .task {
-                    viewModel.refresh()
-                }
-                .navigationTitle(viewModel.city)
-                .navigationBarTitleDisplayMode(.large)
-                .listStyle(.grouped)
-            Button("Refresh") {
-                refresh.toggle()
-                viewModel.refresh()
-            }
-        }
-        
+        List(content: content)
+            .onAppear(perform: viewModel.refresh)
+            .navigationTitle(viewModel.city)
+            .navigationBarTitleDisplayMode(.large)
+            .listStyle(.grouped)
     }
 }
 
 private extension CurrentWeatherView {
-  func content() -> some View {
-    if let viewModel = viewModel.dataSource {
-      return AnyView(details(for: viewModel))
-    } else {
-      return AnyView(loading)
+    func content() -> some View {
+        if let viewModel = viewModel.dataSource {
+            return AnyView(details(for: viewModel))
+        } else {
+            return AnyView(loading)
+        }
     }
-  }
-
-  func details(for viewModel: CurrentWeatherRowViewModel) -> some View {
-    CurrentWeatherRow(viewModel: viewModel)
-  }
-
-  var loading: some View {
-    Text("Loading \(viewModel.city)'s weather...")
-      .foregroundColor(.gray)
-  }
+    
+    func details(for viewModel: CurrentWeatherRowViewModel) -> some View {
+        CurrentWeatherRow(viewModel: viewModel)
+    }
+    
+    var loading: some View {
+        Text("Loading \(viewModel.city)'s weather...")
+            .foregroundColor(.gray)
+    }
 }
 
