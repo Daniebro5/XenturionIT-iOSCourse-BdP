981c11095330a1ae3a2abf4c59dce77d3ff9a861
Danni <daniebro.5@gmail.com>
2023-07-03 09:20:35 -0500

    PLayground de tests

---
+++ Testing/Testing.playground/Contents.swift
@@ -0,0 +1,22 @@
+import XCTest
+
+var greeting = "Hello, Tests!"
+
+/*
+ FIRST (TDD -> Las pruebas primero)
+ 
+ Fast: Deben correr rápidamente
+ Independent/Isolated: No deben compartir estado entre ellas
+ Repeatable: Deberías obtener el mismo resultado independientemente de cuando corras la prueba
+ Self-validating: Completamente automatizada, y la salida debe ser Pass/Fail
+ Timely: TDD -> Idealmente las pruebas van primero
+ 
+ 
+ *** Qué es lo que debo probar? ******
+ 
+ En general, las pruebas deberían cubrir:
+    - Funcionalidad Core: Clases Model, métodos e interacciones de la lógica de negocio
+    - Los workflow más comunes de UI (En conjunto con Analítica)
+    - Casos de Borde (Peticiones de miles de archivos o cero archivos): cubrir casos extremos
+    - Bug Fixes: Para que al introducir nuevo código, no ocurre un Regression
+ */
