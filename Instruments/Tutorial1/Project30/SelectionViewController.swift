150a6e0285bedec1c0f18f9a2b9c4d9198bf0774
Danni <daniebro.5@gmail.com>
2023-07-04 10:56:43 -0500

    fix shadow

--- Instruments/Tutorial1/Project30/SelectionViewController.swift
+++ Instruments/Tutorial1/Project30/SelectionViewController.swift
@@ -55,7 +55,8 @@ class SelectionViewController: UITableViewController {
     }
 
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
 
 		// find the image for this cell, and load its thumbnail
@@ -64,23 +65,26 @@ class SelectionViewController: UITableViewController {
 		let path = Bundle.main.path(forResource: imageRootName, ofType: nil)!
 		let original = UIImage(contentsOfFile: path)!
 
-		let renderer = UIGraphicsImageRenderer(size: original.size)
+        let renderRect = CGRect(origin: .zero,
+                                size: CGSize(width: 90, height: 90))
+        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
 
-		let rounded = renderer.image { ctx in
-			ctx.cgContext.addEllipse(in: CGRect(origin: CGPoint.zero, size: original.size))
-			ctx.cgContext.clip()
+        let rounded = renderer.image { ctx in
+            ctx.cgContext.addEllipse(in: renderRect)
+            ctx.cgContext.clip()
 
-			original.draw(at: CGPoint.zero)
-		}
+            original.draw(in: renderRect)
+        }
 
 		cell.imageView?.image = rounded
 
-		// give the images a nice shadow to make them look a bit more dramatic
-		cell.imageView?.layer.shadowColor = UIColor.black.cgColor
-		cell.imageView?.layer.shadowOpacity = 1
-		cell.imageView?.layer.shadowRadius = 10
-		cell.imageView?.layer.shadowOffset = CGSize.zero
-
+        // give the images a nice shadow to make them look a bit more dramatic
+        cell.imageView?.layer.shadowColor = UIColor.black.cgColor
+        cell.imageView?.layer.shadowOpacity = 1
+        cell.imageView?.layer.shadowRadius = 10
+        cell.imageView?.layer.shadowOffset = CGSize.zero
+        cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
+        
 		// each image stores how often it's been tapped
 		let defaults = UserDefaults.standard
 		cell.textLabel?.text = "\(defaults.integer(forKey: currentImage))"
