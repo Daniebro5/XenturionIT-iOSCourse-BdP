bb9831ca54163a0ea6aad2ef2be698be2d01755f
Danni <daniebro.5@gmail.com>
2023-07-04 21:15:25 -0500

    Starter security

--- Instruments/Tutorial3/Shuffle/NetworkLayer/DataProvider.swift
+++ Instruments/Tutorial3/Shuffle/NetworkLayer/DataProvider.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
 
 enum DataAPI {
@@ -71,7 +39,7 @@ class DataProvider {
   var dataTaskFetchImage: URLSessionDataTask?
   static let sharedInstance = DataProvider()
 
-  func getRandomPicture(completion:@escaping (UIImage?) -> Void) {
+  func getRandomPicture(completion: @escaping (UIImage?) -> Void) {
     dataTaskFetchImage?.cancel()
     guard let url = DataAPI.randomImage.url else {
       return
@@ -92,11 +60,12 @@ class DataProvider {
       }
       completion(image)
     }
+    dataTaskFetchImage?.taskDescription = "RandomImageDownloadTask"
     dataTaskFetchImage?.resume()
   }
 
   func getQOTD(completion:@escaping (QuoteOfTheDay?) -> Void) {
-    dataTaskFetchImage?.cancel()
+    dataTaskFetchQuote?.cancel()
     guard let url = DataAPI.quoteOfTheDay.url else {
       return
     }
@@ -117,6 +86,7 @@ class DataProvider {
       }
       completion(qotd)
     }
+    dataTaskFetchQuote?.taskDescription = "QuoteOfTheDayDownloadTask"
     dataTaskFetchQuote?.resume()
   }
 
